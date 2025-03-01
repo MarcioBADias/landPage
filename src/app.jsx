58b2d8c487d2dfa6d5f36f977a5d8a7be39574ee
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-03-01 17:56:28 -0300

    Estrutura Basica

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Header } from './Components/Header'
+import { HeroSection } from './Components/HeroSection'
+import { Features } from './Components/Features'
+import Testimonials from './Components/Testimonials'
+import { Pricing } from './Components/Pricing'
+import { Footer } from './Components/Footer'
+
 const App = () => {
   return (
-    <>
-      <h1>Boilerplace + React</h1>
-    </>
+    <Router>
+      <Header />
+      <HeroSection />
+      <Features />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </Router>
   )
 }
 
